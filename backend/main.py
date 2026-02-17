6d2ece974deb5286b697ee8195661e1db32c85d3
djdrjeff <jkabuthia@gmail.com>
2026-02-17 14:45:27 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -12,12 +12,29 @@ def get_db():
     finally:
         db.close()
 
+from fastapi import HTTPException, Depends
+from sqlalchemy.orm import Session
+from models import User
+from database import get_db
+
 @app.get("/search")
-def search(email: str, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == email).first()
+def search_user(email: str, db: Session = Depends(get_db)):
+    try:
+        user = db.query(User).filter(User.email == email).first()
+    except Exception as e:
+        print("DB ERROR:", e)
+        raise HTTPException(status_code=500, detail="Database error")
+
     if not user:
-        raise HTTPException(status_code=404, detail="Record not found")
-    return user
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return {
+        "full_name": user.full_name,
+        "email": user.email,
+        "phone": user.phone,
+        "organization": user.organization
+    }
+
 
 @app.post("/update")
 def update(email: str, phone: str, organization: str, db: Session = Depends(get_db)):
